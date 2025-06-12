b229c7c488bd03a6ef30fdbbae18280589cb3e1f
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-06-12 19:55:17 +0200

    feat: support both sysbox and runc

--- gha_runner_create.sh
+++ gha_runner_create.sh
@@ -10,7 +10,6 @@ set -o nounset
 
 function create_sysbox_gha_runner_image() {
     docker rm -f $name >/dev/null 2>&1 || true
-
     docker build -t gha-sysbox-runner-custom:latest .
 }
 
@@ -20,40 +19,78 @@ function create_sysbox_gha_runner_repo {
     name=$1
     org=$2
     repo=$3
-    token=$4
+    runtime=$4
+    token=$5
 
-    echo "Creating repo runner for name=$name org=$org repo=$repo token=$token"
+    echo "Creating repo runner for name=$name org=$org repo=$repo runtime=$runtime token=$token"
 
-    create_sysbox_gha_runner_image
 
-    docker run -d --restart=always \
-        --runtime=sysbox-runc \
-        -e REPO_URL="https://github.com/${org}/${repo}" \
-        -e RUNNER_TOKEN="$token" \
-        -e RUNNER_NAME="$name" \
-        -e RUNNER_GROUP="" \
-        -e LABELS="" \
-        --name "$name" gha-sysbox-runner-custom:latest
+    if [ "$runtime" = "runc" ]; then
+        echo "using runc runtime. prefer using sysbox"
+        create_sysbox_gha_runner_image
+        docker run --privileged --runtimme=runc -d --restart=always \
+            -v /var/run/docker.sock:/var/run/docker.sock
+            -e REPO_URL="https://github.com/${org}/${repo}" \
+            -e RUNNER_TOKEN="$token" \
+            -e RUNNER_NAME="$name" \
+            -e RUNNER_GROUP="" \
+            -e LABELS="$name" \
+            --name "$name" gha-sysbox-runner-custom:latest
+    elif [ "$runtime" = "sysbox" ]; then
+        # With sysbox (recommended)
+        create_sysbox_gha_runner_image
+        docker run -d --restart=always \
+            --runtime=sysbox-runc \
+            -e REPO_URL="https://github.com/${org}/${repo}" \
+            -e RUNNER_TOKEN="$token" \
+            -e RUNNER_NAME="$name" \
+            -e RUNNER_GROUP="" \
+            -e LABELS="" \
+            --name "$name" gha-sysbox-runner-custom:latest
+    else
+        echo "Fatal error: unsupported runtime $runtime"
+    fi
+
 }
 
 function create_sysbox_gha_runner_org {
     name=$1
     org=$2
-    pat=$3
+    runtime=$3
+    pat=$4
 
-    echo "Creating org runner for name=$name org=$org pat=$pat"
+    echo "Creating org runner for name=$name org=$org runtime=$runtime pat=$pat"
 
-    create_sysbox_gha_runner_image
 
-    docker run -d --restart=always \
-        --runtime=sysbox-runc \
-        -e RUNNER_SCOPE="org" \
-        -e ORG_NAME="$org" \
-        -e ACCESS_TOKEN="$pat" \
-        -e RUNNER_NAME="$name" \
-        -e RUNNER_GROUP="" \
-        -e LABELS="" \
-        --name "$name" gha-sysbox-runner-custom:latest
+    if [ "$runtime" = "runc" ]; then
+        echo "using runc runtime. prefer using sysbox"
+        create_sysbox_gha_runner_image
+        # --runtime=sysbox-runc \
+        docker run --privileged --runtime=runc -d --restart=always \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -e RUNNER_SCOPE="org" \
+            -e ORG_NAME="$org" \
+            -e ACCESS_TOKEN="$pat" \
+            -e RUNNER_NAME="$name" \
+            -e RUNNER_GROUP="" \
+            -e LABELS="" \
+            --name "$name" gha-sysbox-runner-custom:latest
+    elif [ "$runtime" = "sysbox" ]; then
+        echo "using sysbox runtime"
+        create_sysbox_gha_runner_image
+        # With sysbox (recommended)
+        docker run -d --restart=always \
+            --runtime=sysbox-runc \
+            -e RUNNER_SCOPE="org" \
+            -e ORG_NAME="$org" \
+            -e ACCESS_TOKEN="$pat" \
+            -e RUNNER_NAME="$name" \
+            -e RUNNER_GROUP="" \
+            -e LABELS="" \
+            --name "$name" gha-sysbox-runner-custom:latest
+    else
+        echo "Fatal error: unsupported runtime $runtime"
+    fi
 }
 
 # Function to display help message
@@ -61,8 +98,8 @@ show_help() {
     echo "Usage: $0 <type> [additional arguments]"
     echo
     echo "<type>:"
-    echo "  repo     Requires name org repo token"
-    echo "  org      Requires name org [pat]"
+    echo "  repo     Requires name org repo <sysbox|runc> token"
+    echo "  org      Requires name org <sysbox|runc> [pat]"
     echo "           If pat is not provided, attempt to read it from github.pat file" 
     echo
     echo "Arguments:"
@@ -92,6 +129,12 @@ function main() {
     # Check the first argument type and validate argument count
     if [ "$1" == "org" ]; then
         if [ $# -lt 4 ]; then
+            echo "Invalid arguments provided"
+            show_help
+            exit 1
+        fi
+                
+        if [ $# -lt 5 ]; then
             if [ -f "github.pat" ]; then
                 PAT_VALUE=$(cat github.pat)
                 echo "Using token from github.pat"
@@ -100,21 +143,21 @@ function main() {
                 show_help
                 exit 1
             fi
-        elif [ $# -eq 4 ]; then
-            PAT_VALUE=$4
+        elif [ $# -eq 5 ]; then
+            PAT_VALUE=$5
         else
             echo "Invalid arguments provided"
             show_help
             exit 1
         fi
-        create_sysbox_gha_runner_org $2 $3 $PAT_VALUE
+        create_sysbox_gha_runner_org $2 $3 $4 $PAT_VALUE
     elif [ "$1" == "repo" ]; then
-        if [ $# -ne 5 ]; then
+        if [ $# -ne 6 ]; then
             echo "Error: invalid arguments provided. $#"
             show_help
             exit 1
         else
-            create_sysbox_gha_runner_repo $2 $3 $4 $5
+            create_sysbox_gha_runner_repo $2 $3 $4 $5 $6
         fi
     else
         echo "Invalid arguments provided"
